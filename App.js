4e803f55d3c2e333ad011459587b0f2591bacce6
borani <boran.sahindal@nordicfc.se>
2023-09-01 16:34:37 +0300

    wip

--- App.js
+++ App.js
@@ -1,3 +1,3 @@
 let paragraphElement = document.createElement("P");
-paragraphElement.innerHTML = "This website is a canvas for my impluses"
+paragraphElement.innerHTML = "This website is a canvas for my impulses"
 document.body.appendChild(paragraphElement)
