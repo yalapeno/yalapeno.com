80f49066262ab95bbc080aff823d9f379da34907
borani <boran.sahindal@nordicfc.se>
2023-09-06 15:46:11 +0300

    wip

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
 let paragraphElement = document.createElement("P");
 paragraphElement.innerHTML = "This website is a canvas for my impulses"
 document.body.appendChild(paragraphElement)
+let paragraphElement2 = document.createElement("P");
+paragraphElement2.innerHTML = "Learning how to flow in the matrix is like learning programming. You get surprised when small things actually work. Escaping it and going binary does not sound fun atm."
+document.body.appendChild(paragraphElement2)
