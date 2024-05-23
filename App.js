ffda5bbb6f8252dbff871c27d484d5e8e6375850
Boran Sahindal <borans@kth.se>
2024-05-23 20:16:09 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 let paragraphElement = document.createElement("P");
-paragraphElement.innerHTML = "This website is a canvas for my impulses"
+paragraphElement.innerHTML = "hellooo"
 document.body.appendChild(paragraphElement)
 let paragraphElement2 = document.createElement("P");
-paragraphElement2.innerHTML = "Learning how to flow in the matrix is like learning programming. You get surprised when small things actually work. Escaping it and going binary does not sound fun atm."
+paragraphElement2.innerHTML = "hellooo"
 document.body.appendChild(paragraphElement2)
